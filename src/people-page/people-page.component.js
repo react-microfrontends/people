582d78052de645b1a22d42e95e00c94ef4eebf26
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-20 12:15:18 -0600

    Tailwind (#5)
    * tailwind 1
    
    * fixing lint error
    
    * fixing button
    
    * tailwind 2

--- src/people-page/people-page.component.js
+++ src/people-page/people-page.component.js
@@ -52,7 +52,7 @@ export default function PeoplePage(props) {
         dispatch({ type: "selectPerson", person });
       }
     }
-  }, [props.location.search]);
+  }, [props.location.search, state.selectedPerson, state.people]);
 
   return (
     <div className="peoplePage" {...scope}>
@@ -60,7 +60,11 @@ export default function PeoplePage(props) {
         <div className="listWrapper">
           {nextPage ? (
             <button
-              className="brand-button margin-bottom-16"
+              className={`mb-8 ${
+                !nextPage || loadingPeople
+                  ? "opacity-50 bg-secondary"
+                  : "bg-warning"
+              } font-bold py-2 px-4 rounded`}
               onClick={fetchMore}
               disabled={!nextPage || loadingPeople}
             >
