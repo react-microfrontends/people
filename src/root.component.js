96d3842c2a7ce8b00ad8bda50a0c01fdb879d7b3
Joel Denning <joeldenning@gmail.com>
2020-06-04 10:11:32 -0600

    Letting single-spa-layout handle positioning (#12)

--- src/root.component.js
+++ src/root.component.js
@@ -4,11 +4,9 @@ import PeoplePage from "./people-page/people-page.component.js";
 
 export default function Root(props) {
   return (
-    <div className="mt-16">
-      <BrowserRouter>
-        <Route path="/people/:personId" component={PeoplePage} />
-        <Route path="/people" component={PeoplePage} exact />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Route path="/people/:personId" component={PeoplePage} />
+      <Route path="/people" component={PeoplePage} exact />
+    </BrowserRouter>
   );
 }
