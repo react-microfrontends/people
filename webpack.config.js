a9079ad169d3301b37aec1bc01c23957252317f6
Joel Denning <joeldenning@gmail.com>
2020-10-10 19:21:33 -0600

    Standalone mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
-const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StandaloneSingleSpaPlugin = require("standalone-single-spa-webpack-plugin");
 
-module.exports = (webpackConfigEnv) => {
+module.exports = (webpackConfigEnv = {}) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "react-mf",
     projectName: "people",
     webpackConfigEnv,
   });
 
-  const rxjsExternals = {
-    externals: [/^rxjs\/?.*$/],
-  };
-
-  return webpackMerge.smart(defaultConfig, rxjsExternals, {
+  const config = webpackMerge.smart(defaultConfig, {
     // customizations go here
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new StandaloneSingleSpaPlugin({
+        appOrParcelName: "@react-mf/people",
+        importMapUrl: new URL(
+          "https://react.microfrontends.app/importmap.json"
+        ),
+        activeWhen: ["/people"],
+        isParcel: false,
+        disabled: !webpackConfigEnv.standalone,
+      }),
+    ],
+    externals: [/^rxjs\/?.*$/],
   });
+
+  return config;
 };
