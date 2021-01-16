4d0af0a673764fa9b311f9f98163c88be9af9426
Joel Denning <joeldenning@gmail.com>
2021-01-15 19:42:54 -0700

    Fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const StandaloneSingleSpaPlugin = require("standalone-single-spa-webpack-plugin");
 
 module.exports = (webpackConfigEnv = {}) => {
   const defaultConfig = singleSpaDefaults({
